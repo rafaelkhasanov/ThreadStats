8b1e71e9f0faf6b565f04185f6b986f002f64c9d
Рафаэль Хасанов <rap1raf@gmail.com>
2019-05-22 00:56:30 +0300

    Add project files.

---
+++ ThreadStats/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primary Thread stats");
+
+            //Получить имя текущего потока
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+            //Показать детали обслуживающего домена приложения и контекста
+            Console.WriteLine($"Name of current AppDomain: {Thread.GetDomain().FriendlyName}");
+            Console.WriteLine($"Id of current Context: {Thread.CurrentContext.ContextID}");
+            //Вывести некоторую статистику о текущем потоке
+            Console.WriteLine($"Thread Name: {primaryThread.Name}");
+            Console.WriteLine($"Has thread started?: {primaryThread.IsAlive}");
+            Console.WriteLine($"Priority Level: {primaryThread.Priority}");
+            Console.WriteLine($"Thread State: {primaryThread.ThreadState}");
+            Console.ReadLine();
+        }
+    }
+}
